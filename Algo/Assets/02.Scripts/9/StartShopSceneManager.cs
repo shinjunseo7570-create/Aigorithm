a992660e93a8e15984e328a6e2ff7db5213f0b3b
VIOLEILLICIT <202211094@sangmyung.kr>
2025-11-27 17:25:44 +0900

    hj

--- Algo/Assets/02.Scripts/9/StartShopSceneManager.cs
+++ Algo/Assets/02.Scripts/9/StartShopSceneManager.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class StartShopSceneManager : MonoBehaviour
 {
     [Header("Scene의 DialogueManager 연결")]
     public DialogueManager dialogueManager;
 
+    [Header("플레이어 연결")]
+    public PlayerStatus player;
+
     [Header("대사 설정")]
     [TextArea(3, 10)] // 인스펙터에서 입력창을 넓게 보여줍니다.
     public string startMessage = "...";
+
+    [Header("스탯 표시 텍스트 연결")]
+    public TextMeshProUGUI statusText;
 
     void Start()
     {
@@ -20,5 +27,14 @@ public class StartShopSceneManager : MonoBehaviour
         {
             Debug.LogWarning("SceneStartDialogue: DialogueManager가 연결되지 않았습니다!");
         }
+
+        ShowCurrentStatus();
+    }
+
+    public void ShowCurrentStatus()
+    {
+        statusText.text = ($"체력: {player.currentHealth}/{player.maxHealth}\n" +
+            $"스태미나: {player.currentStamina}/{player.maxStamina}\n" +
+            $"공격력: {player.strength}\n");
     }
 }
